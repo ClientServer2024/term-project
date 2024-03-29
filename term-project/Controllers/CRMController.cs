294ea550dac3a1570c69b7cd453737079302f614
Bardia <bilbeigitimouri@my.bcit.ca>
2024-03-29 00:13:46 -0700

    Added empty files for the framework.

---
+++ term-project/Controllers/CRMController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace term_project.Controllers
+{
+    public class CRMController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
