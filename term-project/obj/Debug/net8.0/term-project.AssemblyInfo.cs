20c820b0562f2eabfc31c98a4d224e06460c278a
Cassidy Boilley <cboilley@my.bcit.ca>
2024-04-05 11:55:10 -0700

    fix merges

--- term-project/obj/Debug/net8.0/term-project.AssemblyInfo.cs
+++ term-project/obj/Debug/net8.0/term-project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("term-project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35a52a89e7f62596a05dde1c9fea1c930e00343b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65b9a0946a5d5987e0f5dbb6ef086a4983cb985d")]
 [assembly: System.Reflection.AssemblyProductAttribute("term-project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("term-project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
