ca07a830ed794a0c8873f4afda10b2af9da3153c
GursidhSandhu <gsandhu168@my.bcit.ca>
2024-04-05 15:00:10 -0700

    added email column to reflect db

--- term-project/Models/CRMModels/Applicant.cs
+++ term-project/Models/CRMModels/Applicant.cs
@@ -31,6 +31,6 @@ namespace term_project.Models.CRMModels
         public string Status { get; set; }
         
         [Column("email")]
-        public string Email { get; set; }
+        public string Email { get; set; } 
     }
 }
